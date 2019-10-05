7dc4e9db9111e98809675d0d6c21e13a26c476c9
Jacob Maxton <jacob-maxton@web.de>
2019-10-05 22:11:21 +0200

    changed the arginput system of the main tts file. good luck with mergeconflicts ;)

--- synthi-tts.py
+++ synthi-tts.py
@@ -3,25 +3,26 @@
 from subprocess import Popen, PIPE
 import sys
 import os
+import argparse
+
+#using argparse to get the information about language, folder and text from CLI
+parser = argparse.ArgumentParser("synthi-tts <text>")
+parser.add_argument('text', metavar='<text>', type=str, nargs='*', help='the text the program has to pronounce.')
+parser.add_argument('-f', dest='folder', action='store', help='the folder from which the program loads the phonemes.')
+parser.add_argument('-l', dest='language', action='store', default='english', help='the language the program uses to determine the pronounciation.\nDefault: english')
+parser.add_argument('--silence_missing_phonemes', dest='missing_phonemes_print', action='store_true', default=False, help="use this flag if you don't want to see messages telling you which parts of the phenatics the program does'nt know.")
+
 
 # Getting folder and files from CLI
-folder = ""
+args = parser.parse_args()
 text = ""
-if len(sys.argv) > 2:
-    folder = sys.argv[1]
-    if not folder.endswith("/"):
-        folder = folder + "/"
-        
-    text = sys.argv[2]
-else:
-    print('usage: python3 synthi-tts.py <voice sample folder> "<text>"', file=sys.stderr)
-    sys.exit()
-
-
+for word in args.text:
+    text += " " + word
+text = text[1:]
 # Getting language from espeak
-process = Popen(['espeak', '-q', '-x', '"' + text + '"'], stdout=PIPE, stderr=PIPE)
+process = Popen(['espeak', '-q', '-x', '"' + text + '" -v ' + args.language.lower()], stdout=PIPE, stderr=PIPE)
 stdout, stderr = process.communicate()
-phonetic = stdout.decode('utf-8').strip()[5:]
+phonetic = stdout.decode('utf-8').striparsetparsetp()[5:]
 #print(phonetic)
 
 # create e_map from espeak-gentle translation
@@ -31,7 +32,7 @@ with open("espeak-to-gentle", encoding="utf-8") as f:
     for line in f.readlines():
         phon, audio = line.split("=")
         audio = audio.split(" ")
-        audio = [(folder + a.strip() + ".mp3") for a in audio]
+        audio = [(args.folder + '/' + a.strip() + ".mp3") for a in audio]
         if len(phon) and not phon in e_map:
             e_map[phon] = audio
 
@@ -58,7 +59,8 @@ while i < len(phonetic):
         files = files + e_map[phonetic[i:i+1]]
         i = i + 1
     else:
-        print(phonetic[i] + ": not found", file=sys.stderr)
+        if not args.missing_phonemes_print:
+            print(phonetic[i] + ": not found", file=sys.stderr)
         i = i + 1
 
 # Assembly of ffmpeg command
